4f51ebd03518695af105a90dd330ef7e579f9d4a
ianrp <irpennin@gmail.com>
2023-03-10 23:42:28 -0500

    Replace read status and delete buttons with icons

--- script.js
+++ script.js
@@ -37,16 +37,27 @@ function updateTable() {
     row.appendChild(pagesCell);
 
     const readCell = document.createElement('td');
+    readCell.classList.add('read-cell');
     const readButton = document.createElement('button');
-    if (myLibrary[i].read) readButton.textContent = 'Read';
-    else readButton.textContent = 'Unread';
+    if (myLibrary[i].read) {
+      readButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>checkbox-marked</title><path d="M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z" /></svg>';
+      readButton.classList.add('read-status');
+    } else {
+      readButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close-box</title><path d="M19,3H16.3H7.7H5A2,2 0 0,0 3,5V7.7V16.4V19A2,2 0 0,0 5,21H7.7H16.4H19A2,2 0 0,0 21,19V16.3V7.7V5A2,2 0 0,0 19,3M15.6,17L12,13.4L8.4,17L7,15.6L10.6,12L7,8.4L8.4,7L12,10.6L15.6,7L17,8.4L13.4,12L17,15.6L15.6,17Z" /></svg>';
+      readButton.classList.add('unread-status');
+    }
     readButton.addEventListener('click', () => toggleReadStatus(i));
     readCell.appendChild(readButton);
     row.appendChild(readCell);
 
     const removeCell = document.createElement('td');
     const removeButton = document.createElement('button');
-    removeButton.textContent = 'X';
+
+    const deleteIcon = document.createElement('img');
+    deleteIcon.src = 'icons/delete-outline.svg';
+    deleteIcon.classList.add('hidden');
+    removeButton.appendChild(deleteIcon);
+
     removeButton.classList.add('remove-book');
     removeButton.addEventListener('click', () => removeBook(i));
     removeCell.appendChild(removeButton);
